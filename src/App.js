6f55645089e86ebff9f12d4aff85b6f03e63c0d7
Abimael Espinal <abimaelespinal@gmail.com>
2019-06-09 15:16:07 -0400

    added time

--- src/App.js
+++ src/App.js
@@ -8,24 +8,47 @@ class App extends Component {
 
   state={
     data:[],
+    currentTime: String(new(Date))
   }
 
 async componentDidMount(){
-  console.log("Data",this.state.data)
+  
+
 const {data} = await axios.get(" http://api.giphy.com/v1/gifs/trending?api_key=y87yq4gzawJHGxKSAkKIOvY51Kwc8V0V")
   this.setState({data:data.data})
 }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.currentTime !== this.state.currentTime
+  }
+
+  // async componentDidUpdate(prevProps, prevState) {
+  //   if(prevState.data === this.state.data){
+  //   const {data} = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${"hello"}&api_key=y87yq4gzawJHGxKSAkKIOvY51Kwc8V0V`)
+  //   this.setState({data:data.data})
+  //   }
+  // }
+
   handleSearchData = async(search) => {
     const {data} = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=y87yq4gzawJHGxKSAkKIOvY51Kwc8V0V`)
       this.setState({data:data.data})
   }
 
+  displayTime = () => {
+    setInterval( () => {
+      this.setState({currentTime: String(new(Date))
+      }) 
+    },1000)
+
+   return this.state.currentTime
+  }
+
   handleTrendingGif = () => {
-    console.log(this.state.data)
+
     return(
       this.state.data.map((data, index) =>
       <div className="AppSub">
-        <img src={data.images.original.url}/>
+        <img  key={index} src={data.images.original.url}/>
       </div>)
     )
   }
@@ -33,6 +56,8 @@ const {data} = await axios.get(" http://api.giphy.com/v1/gifs/trending?api_key=y
   return (
     <React.Fragment>
     <header className="App-header">
+      {this.displayTime()}
+      
     <SearchField
     handleSearch={this.handleSearchData}
     />
